b7e1ac1ad944e2190e1ab32db2c82f4c038a555b
JoaoLuizRSilva <silvajoaoluiz360@gmail.com>
2021-09-28 20:08:29 -0300

    Lab_dom

--- lab01/ex05/scripts.js
+++ lab01/ex05/scripts.js
@@ -1,7 +1,6 @@
 let limit = 1000;
 for (let number = 2; number <= limit; number++){
     let isCousin = true;
-
     for (let divider = 2; divider < number; divider++){
         if (number % divider === 0){
             isCousin = false;
