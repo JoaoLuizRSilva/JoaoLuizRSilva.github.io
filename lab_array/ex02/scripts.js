4321cf34206cfcf5544d2d44de56aed4c851a4e7
JoaoLuizRSilva <silvajoaoluiz360@gmail.com>
2021-09-28 20:11:44 -0300

    Alguma mudanças

--- lab_array/ex02/scripts.js
+++ lab_array/ex02/scripts.js
@@ -1,41 +1,17 @@
-<<<<<<< Updated upstream
 function numeroPrimo(n1, n2) {
-    let numero = new Array();
-    for (var c = n1; c <= n2; c++) {
-      if (primo(c)){
-        numero.push(c);
-      }
+  let numero = new Array();
+  for (var c = n1; c <= n2; c++) {
+    if (primo(c)){
+      numero.push(c);
     }
-    return numero;
   }
-  function primo(n2) {
-    for(let c = 2; c <n2; c++)
-      if(n2 % c === 0) {
-          return false
-      };
-    return n2 > 1;
-  }
-  console.log(numeroPrimo(5, 80));
-=======
-/* function cousin(limit) {
-    for (let number = 2; number <= limit; number++){
-        let isCousin = true;
-        for (let divider = 2; divider < number; divider++){
-            if (number % divider === 0){
-                isCousin = false;
-                break;
-            }
-        }
-        if (isCousin){
-            console.log(number);
-        }
-    }
+  return numero;
 }
-cousin(50) */
-let number = 2, divider = 2;
-function cousin (limit) {
-    while (number <= limit ) {
-        let isCousin = true;
-    }
+function primo(n2) {
+  for(let c = 2; c <n2; c++)
+    if(n2 % c === 0) {
+        return false
+    };
+  return n2 > 1;
 }
->>>>>>> Stashed changes
+console.log(numeroPrimo(5, 80));
