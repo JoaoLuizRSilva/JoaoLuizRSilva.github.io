dc8843d04603fd2ffee25f1548cdda54c2c4de18
JoaoLuizRSilva <silvajoaoluiz360@gmail.com>
2021-09-28 20:10:34 -0300

    Alguma mudanças

--- lab_array/ex02/scripts.js
+++ lab_array/ex02/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function numeroPrimo(n1, n2) {
     let numero = new Array();
     for (var c = n1; c <= n2; c++) {
@@ -15,3 +16,26 @@ function numeroPrimo(n1, n2) {
     return n2 > 1;
   }
   console.log(numeroPrimo(5, 80));
+=======
+/* function cousin(limit) {
+    for (let number = 2; number <= limit; number++){
+        let isCousin = true;
+        for (let divider = 2; divider < number; divider++){
+            if (number % divider === 0){
+                isCousin = false;
+                break;
+            }
+        }
+        if (isCousin){
+            console.log(number);
+        }
+    }
+}
+cousin(50) */
+let number = 2, divider = 2;
+function cousin (limit) {
+    while (number <= limit ) {
+        let isCousin = true;
+    }
+}
+>>>>>>> Stashed changes
